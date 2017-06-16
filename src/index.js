4cdad704eb9d3a3b43929650b5902bf58a549b3c
Matei Radu <matei.radu.92@gmail.com>
2017-06-16 22:37:54 +0200

    - added basic testing - removed unused assets and components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*
+ * Unigram Website
+ * Copyright © 2016-2017 Unigram <team@unigram.me>
+ * Licensed under MIT 
+ * (https://github.com/UnigramDev/unigram.me/blob/master/LICENSE)
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from './Navigation';
