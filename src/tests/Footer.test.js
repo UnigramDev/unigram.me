ce14ed15f0a38d5a58ef972803f6df79bc9d027f
Matei Radu <matei.radu.92@gmail.com>
2017-06-22 17:22:38 +0200

    - adapted tests to accommodate react-router

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -6,9 +6,10 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import Footer from '../common/components/Footer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Footer />, div);
+  ReactDOM.render(<Router><Footer /></Router>, div);
 });
--- src/tests/Navigation.test.js
+++ src/tests/Navigation.test.js
@@ -6,9 +6,10 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from '../common/components/Navigation';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Navigation />, div);
+  ReactDOM.render(<Router><Navigation /></Router>, div);
 });
