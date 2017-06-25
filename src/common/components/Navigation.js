2c715dd72ff95766597a436299527d068160e9c1
Matei Radu <matei.radu.92@gmail.com>
2017-06-25 12:59:54 +0200

    - navigation CSS refactoring - fixes #13, #11

--- src/common/components/Navigation.js
+++ src/common/components/Navigation.js
@@ -15,42 +15,40 @@ import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () => (
-  <div className="navigation-root">
-    <nav className="navbar fixed-top navbar-toggleable-md" id="main-nav">
-      <div className="container">
-        <Link className="navbar-brand" to={'/'}>
-          <img src="img/buttons/unigram.svg" alt="Unigram Logo"/>Unigram
-        </Link>
-        <div className="collapse navbar-collapse" id="navbarExample">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#features">
-                <i className="mdl2 mdl2-favorite-list" aria-hidden="true"></i>Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={Const.DownloadLink}>
-                <i className="mdl2 mdl2-download" aria-hidden="true"></i>Download
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#team">
-                <i className="mdl2 mdl2-people" aria-hidden="true"></i>Community
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to={'/support'}>
-                <i className="mdl2 mdl2-accounts" aria-hidden="true"></i>Support
-              </Link>
-            </li>
-          </ul>
-        </div>
+  <nav className="navbar fixed-top navbar-toggleable-md">
+    <div className="container">
+      <Link className="navbar-brand" to={'/'}>
+        <img src="img/buttons/unigram.svg" alt="Unigram Logo"/>Unigram
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarExample">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to={'/features'}>
+              <i className="mdl2 mdl2-favorite-list" aria-hidden="true"></i>Features
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href={Const.DownloadLink}>
+              <i className="mdl2 mdl2-download" aria-hidden="true"></i>Download
+            </a>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/community'}>
+              <i className="mdl2 mdl2-people" aria-hidden="true"></i>Community
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/support'}>
+              <i className="mdl2 mdl2-accounts" aria-hidden="true"></i>Support
+            </Link>
+          </li>
+        </ul>
       </div>
-      <button className="navbar-toggler navbar-toggler-left" data-toggle="collapse" data-target="#navbarExample" type="button" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
-        <img src="img/buttons/menu.svg" alt=""/>
-      </button>
-    </nav>
-  </div>
+    </div>
+    <button className="navbar-toggler navbar-toggler-left" data-toggle="collapse" data-target="#navbarExample" type="button" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+      <img src="img/buttons/menu.svg" alt="Menu icon"/>
+    </button>
+  </nav>
 );
 
 export default Navigation;
