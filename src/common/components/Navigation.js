69c479224613d3c58fc69be05112c81efc8cd2d6
Matei Radu <matei.radu.92@gmail.com>
2017-07-09 00:17:22 +0200

    - menu closes when section clicked

--- src/common/components/Navigation.js
+++ src/common/components/Navigation.js
@@ -9,46 +9,68 @@
  * @file Navigation bar component.
  * @author Matei Bogdan Radu <matei.radu.92@gmail.com>
  */
-import React from 'react';
+import React, { Component } from 'react';
 import * as Const from '../data/Constants';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
-const Navigation = () => (
-  <nav className="navbar fixed-top navbar-toggleable-md">
-    <div className="container">
-      <Link className="navbar-brand" to={'/'}>
-        <img src="img/buttons/unigram.svg" alt="Unigram Logo"/>Unigram
-      </Link>
-      <div className="collapse navbar-collapse" id="navbarExample">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to={'/features'}>
-              <i className="mdl2 mdl2-favorite-list" aria-hidden="true"></i>Features
-            </Link>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href={Const.DownloadLink}>
-              <i className="mdl2 mdl2-download" aria-hidden="true"></i>Download
-            </a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to={'/community'}>
-              <i className="mdl2 mdl2-people" aria-hidden="true"></i>Community
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to={'/support'}>
-              <i className="mdl2 mdl2-accounts" aria-hidden="true"></i>Support
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <button className="navbar-toggler navbar-toggler-left" data-toggle="collapse" data-target="#navbarExample" type="button" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
-      <img src="img/buttons/menu.svg" alt="Menu icon"/>
-    </button>
-  </nav>
-);
+
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleMenu= this.toggleMenu.bind(this);
+    this.state = {
+      active: false,
+    };
+  }
+
+  /**
+   * Toggle the menu on small screens.
+   */
+  toggleMenu() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  };
+
+  render() {
+    return (
+      <nav className="navbar fixed-top navbar-toggleable-md">
+        <div className="container">
+          <Link className="navbar-brand" to={'/'}>
+            <img src="img/buttons/unigram.svg" alt="Unigram Logo"/>Unigram
+          </Link>
+          <div className={this.state.active ? 'navbar-collapse collapse show': 'navbar-collapse collapse'} id="navbarExample">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={'/features'} onClick={this.toggleMenu}>
+                  <i className="mdl2 mdl2-favorite-list" aria-hidden="true"></i>Features
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href={Const.DownloadLink} onClick={this.toggleMenu}>
+                  <i className="mdl2 mdl2-download" aria-hidden="true"></i>Download
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/community'} onClick={this.toggleMenu}>
+                  <i className="mdl2 mdl2-people" aria-hidden="true"></i>Community
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/support'} onClick={this.toggleMenu}>
+                  <i className="mdl2 mdl2-accounts" aria-hidden="true"></i>Support
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <button className="navbar-toggler navbar-toggler-left" onClick={this.toggleMenu} type="button" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+          <img src="img/buttons/menu.svg" alt="Menu icon"/>
+        </button>
+      </nav>
+    );
+  }
+}
 
 export default Navigation;
