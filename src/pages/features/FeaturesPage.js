b189552864eff90485f855819fc37eae78f8374a
Matei Radu <matei.radu.92@gmail.com>
2017-07-28 16:19:28 +0200

    - adjusted Feature padding - added dummy images for Feature page

--- src/pages/features/FeaturesPage.js
+++ src/pages/features/FeaturesPage.js
@@ -16,9 +16,9 @@ import './FeaturesPage.css';
 
 const FeaturesPage = () => (
   <div className="featurepage-root">
-    <Feature title="Calls" descr="With HD calls, voices sound clearer and closer. And with video calls, you can have face-to-face conversations with friends and family wherever they are." horizontal={true} pic="img/devices/dummy.png"/>
-    <Feature title="Share Media" descr="With a built-in camera, Messenger lets you snap and shoot moments as they happen. You can even add drawings or text to personalize your photos." horizontal={false} pic="img/devices/dummy.png"/>
-    <Feature title="GIFs & Stickers" descr="Send delightful, silly, cute, weird and expressive stickers to say just about anything. Search for stickers to match your mood or explore categories like Happy and In Love." horizontal={true} pic="img/devices/dummy.png"/>
+    <Feature title="Calls" descr="With HD calls, voices sound clearer and closer. And with video calls, you can have face-to-face conversations with friends and family wherever they are." horizontal={true} pic="img/devices/dummy-mobile.png"/>
+    <Feature title="Share Media" descr="With a built-in camera, Messenger lets you snap and shoot moments as they happen. You can even add drawings or text to personalize your photos." horizontal={false} pic="img/devices/dummy-desktop.png"/>
+    <Feature title="GIFs & Stickers" descr="Send delightful, silly, cute, weird and expressive stickers to say just about anything. Search for stickers to match your mood or explore categories like Happy and In Love." horizontal={true} pic="img/devices/dummy-mobile.png"/>
   </div>
 );
 
